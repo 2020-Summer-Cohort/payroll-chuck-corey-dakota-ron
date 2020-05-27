4e218cf16343099089445b4d34f131836b4eb359
Corey T Cox <coxcorey19@gmail.com>
2020-05-27 15:10:03 -0400

    WIP listEmployees method

---
+++ CompanyPayroll/PayrollEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyPayroll
+{
+    class PayrollEmployee
+    {
+        public int employeeId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public double paycheckTotal { get; set; }
+
+       public string GetEmployeeName()
+        {
+            return lastName + ", " + firstName;
+        }
+
+    }
+}
--- CompanyPayroll/Program.cs
+++ CompanyPayroll/Program.cs
@@ -7,6 +7,11 @@ namespace CompanyPayroll
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            PayrollEmployee payrollEmployeeWorker = new PayrollEmployee();
+            payrollEmployeeWorker.lastName = "Givens";
+            payrollEmployeeWorker.firstName = "Hannah";
+            Console.WriteLine(payrollEmployeeWorker.GetEmployeeName());
+                
         }
     }
 }
