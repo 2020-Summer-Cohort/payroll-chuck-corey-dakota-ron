922521b0cd69bca03bb77db3aeb10052f26c7bb2
Dakota <Dakotawhittle1203@gmail.com>
2020-05-28 14:09:45 -0400

    added PayrollSales class

---
+++ CompanyPayroll/PayrollSales.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyPayroll
+{
+    public class PayrollSales:PayrollEmployee
+    {
+        public double SalesComission { get; set; }
+        public double PayPeriodEarnings { get; set; }
+        public void RecieveBonus()
+        {
+
+        }
+        public void SetSalesComission(double salesComissionAmount)
+        {
+            SalesComission = salesComissionAmount;
+        }
+    }
+    
+}
